0647127bbf6849acd1592853cdf32c9a89fab6d2
avramenkomy <mika_lokomotiv@mail.ru>
2020-10-27 16:38:28 +0300

    C8-addToDoList: add Todo list and Bascket app in project

--- my-first-project/src/router/index.js
+++ my-first-project/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Todo from '../views/Todo'
+import Bascket from '../views/Bascket'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/todo',
+    name: 'todo',
+    component: Todo
+  },
+  {
+    path: '/bascket',
+    name: 'bascket',
+    component: Bascket
   },
   {
     path: '/about',
---
+++ my-first-project/src/views/Todo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <input type="text" v-model="todoText" placeholder="Введите задачу">
+
+        <button @click="add" :disabled="!todoText">Добавить</button>
+        <button @click="deleteAll">Удалить все</button>
+
+        <div v-for="(task, key) in tasks" :key="key">
+            <span>{{ key + 1 }}. {{ task.text }}</span>
+            <button @click="deleteElement(key)">Удалить</button>
+        </div>
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "Todo",
+        data() {
+            return {
+                todoText: '',
+                tasks: [],
+            }
+        },
+        methods: {
+            add() {
+                this.tasks.push({ text: this.todoText});
+                this.todoText = '';
+            },
+            deleteAll() {
+                this.tasks = [];
+            },
+            deleteElement(key) {
+                this.tasks.splice(key, 1);
+            },
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
